8d21217cca4b6b423a96a43314609e92c0daa546
EOGames <113043641+EOGames@users.noreply.github.com>
2023-03-03 12:49:45 +0530

    Just Updates

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,9 +1,20 @@
 import React from "react";
+import '../../Custom.css';
 
 const ImageLinkForm = function()
 {
     return (
-        <div></div>
+        <div id="linkFormBG">
+            <p id="textMsg">
+                {'Please Enter A Photo Link To Detect Faces'}
+            </p>
+            
+
+            <div id="">
+                <input id="pic_link"  type={"text"} placeholder ={'Please enter A Link To Photo'} />
+                <button id="btn" >Detect Faces</button>
+            </div>
+        </div>
     );
 }
 export default ImageLinkForm;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,20 @@
+import React from "react";
+import '../../Custom.css';
+
+const Rank = function()
+{
+    return(
+        <div>
+
+            <div>
+                <p id="rankText"> {'Your Rank is Noob'}</p>
+            </div>
+
+            <div>
+                <p id="rankText"> {'#1'}</p>
+            </div>
+        
+        </div>
+    );
+}
+export default Rank;
